8a5e06f645655f92c9574b9e39d6d7ec9ceb3484
Kristian Mesic <kristian.mesic303@gmail.com>
2023-12-12 17:13:25 +0100

    Add project files.

---
+++ BlazorServerApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+
+        private DbSet<Employee> Employees { get; set; }
+    }
+}
